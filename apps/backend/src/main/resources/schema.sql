6349c22ed3a86e9dea1c576f0eef2bd854184446
rockit10 <rockito10.mfd@gmail.com>
2026-02-26 09:49:02 -0300

    chore: auto load schema

--- apps/backend/src/main/resources/schema.sql
+++ apps/backend/src/main/resources/schema.sql
@@ -1,19 +1,19 @@
 
 CREATE EXTENSION IF NOT EXISTS citext;
 
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
                        username CITEXT NOT NULL PRIMARY KEY,
                        password VARCHAR(500) NOT NULL,
                        enabled BOOLEAN NOT NULL,
                        id SERIAL NOT NULL
 );
 
-CREATE TABLE authorities (
+CREATE TABLE IF NOT EXISTS authorities (
                              username CITEXT NOT NULL,
                              authority CITEXT NOT NULL,
                              CONSTRAINT fk_authorities_users
                                  FOREIGN KEY (username) REFERENCES users (username)
 );
 
-CREATE UNIQUE INDEX ix_auth_username
+CREATE UNIQUE INDEX IF NOT EXISTS ix_auth_username
     ON authorities (username, authority);
