13df2ebf54596e9b7154a9d94643fd6c857be257
rockit10 <rockito10.mfd@gmail.com>
2026-03-02 11:41:56 -0300

    fix: remove error on user info

--- apps/backend/src/main/java/com/mensajeria/controller/dto/userinfo/UserInfo.java
+++ apps/backend/src/main/java/com/mensajeria/controller/dto/userinfo/UserInfo.java
@@ -6,15 +6,10 @@ import lombok.Setter;
 @Getter
 @Setter
 public class UserInfo {
-    private String error;
     private final UserInfoData data;
 
     public UserInfo(UserInfoData data) {
         this.data = data;
     }
 
-    public UserInfo(String error, UserInfoData data) {
-        this.error = error;
-        this.data = data;
-    }
 }
